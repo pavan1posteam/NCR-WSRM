34fc99c175d671370d5ec4e23c0083a81df0ff7d
pavan1posteam <pavan.kumar@bottlecapps.com>
2025-11-20 12:16:55 +0530

    STAGED COMMIT

--- NCR(WSRM)/Form1.cs
+++ NCR(WSRM)/Form1.cs
@@ -117,7 +117,7 @@ namespace NCR_WSRM_
             text4 += " LEFT JOIN IM_CATEG_COD ICC ON I.CATEG_COD = ICC.CATEG_COD ";
             text4 += " WHERE B.BARCOD != '' AND B.BARCOD IS NOT NULL ";
             //text4 = text4 + " AND I.Loc_ID = '" + fTPLocation + "'" + text2 + text;
-            text4 = text4 + " AND p.STK_LOC_ID = '" + fTPLocation + "'"+ " AND I.Loc_ID = '" + fTPLocation + "'" + text2 + text;//Changed on 10/15/2025
+            text4 = text4 + " AND p.STK_LOC_ID = '" + fTPLocation + "'"+ " AND I.Loc_ID = '" + fTPLocation + "'" + text2 + text;//Changed on 10/15/2025  
             SqlConnection sqlConnection = new SqlConnection(setting.ConnectionString);
             SqlCommand sqlCommand = new SqlCommand(text4, sqlConnection);
             sqlConnection.Open();
@@ -332,9 +332,9 @@ namespace NCR_WSRM_
             //query += " LEFT JOIN IM_CATEG_COD ICC ON I.CATEG_COD = ICC.CATEG_COD ";
             //query += " WHERE B.BARCOD != '' AND B.BARCOD IS NOT NULL ";
             //query += " AND I.Loc_ID = '" + fTPLocationNCR + "'" + text3NCR + text2NCR;
-            //#endregion
+            #endregion
 
-            //#region Yankee's Stores
+            #region Yankee's Stores
             ////string query = "SELECT DISTINCT '" + storeId + "' AS StoreId, '#'+LTRIM(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(UPC, '/', ''), ',', ''), '=', ''), '.', ''), '`', '')) AS Upc , CAST(QTY AS INT) AS Qty, '#'+LTRIM(ITEM_NO) AS Sku, ";
             ////query += " ISNULL(UNIT,'') AS pack, ISNULL(UNIT,'') AS uom, REPLACE(LTRIM(RTRIM(NAME)), ',', ' ') AS StoreProductName, REPLACE(LTRIM(RTRIM(NAME)), ',', ' ') AS StoreDescription, ";
             ////query += " ISNULL(PRICE,0) AS Price, 0 AS sprice, '' as startdate, '' as enddate, '" + ftpTax + "' AS Tax,  ";
